47a840a4cd4b7658ff40a4192fb773ee378ed03b
Ephraimiyanda <iyandaephraim@gmail.com>
2023-05-02 13:02:57 -0900

    new commit

--- src/login-signup/loginAndSignupPage.jsx
+++ src/login-signup/loginAndSignupPage.jsx
@@ -2,13 +2,14 @@ import SignUp from "./sign-up";
 import Login from "./login";
 import {BrowserRouter as Router,Switch, Route} from "react-router-dom"
 import { Link } from "react-router-dom";
+import welcomePic from "../../images/welcome-pic.png"
 function showLoginSignUp(){
     return(
 <div className="signUpLogin">
 <div >
         <div className="img">
             <h1>Welcome to</h1>
-            <img className="img-signupLogin" src="./images/signup/Rectangle 6 (1).png" alt="" height="710px" width="600px"></img>
+            <img className="img-signupLogin" src={welcomePic} alt="" ></img>
             <h2>AGRIFIED</h2>
             <h3>E-FARM</h3>
         </div>
@@ -19,7 +20,7 @@ function showLoginSignUp(){
                 <Link to="/login"><button>farmer</button></Link>
             </div>
         </div>
-</div>
+    </div>
 
        <div>
     
